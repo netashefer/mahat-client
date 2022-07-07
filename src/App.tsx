961a9588aec9d1b32db26156165d9b02162b4c13
Yahell <yahellodev@gmail.com>
2022-07-07 18:36:44 +0300

    add a bit of styling
    sorry I had to!!!

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Dashboard from "./components/Dashboard/Dashboard";
 import ManagerPanel from "./components/ManagerPanel/ManagerPanel";
 import { FullDataInstanceInfo, Table, TableDictionary } from "./types/data";
 import './App.scss';
+import { AppBar, Toolbar } from "@mui/material";
 
 const App = () => {
   const [tableDictionary, setTableDictionary] = useState<TableDictionary>({});
@@ -29,7 +30,11 @@ const App = () => {
 
   return (
     <div className="App" id="app">
-      <div className="page-title">Yahel and Neta</div>
+       <AppBar position="static" className="app-bar">
+        <Toolbar>
+          Yahel and Neta 
+        </Toolbar>
+      </AppBar>
       <div className="wrapper">
         <Dashboard />
         <ManagerPanel
