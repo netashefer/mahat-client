7d72461d94faa95f5dcc04154363f5a652fcf3be
neta <neta111.shefer@gmail.com>
2022-10-05 23:32:49 +0300

    route name

--- src/communication/excelCommunicator.ts
+++ src/communication/excelCommunicator.ts
@@ -7,7 +7,7 @@ class ExcelCommunicator extends Communicator {
 
     async addExcelDataSource(excelDataSource: { table: Table, displayName: string, dashboardId: string; }) {
         try {
-            return await requestProvider.post<string>(this.getFullURL("excel/add"), excelDataSource);
+            return await requestProvider.post<string>(this.getFullURL("excel/addDataSource"), excelDataSource);
         } catch (e) {
             console.error(e);
         }
